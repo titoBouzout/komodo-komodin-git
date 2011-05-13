cd131ecf63e31e8d3d4dff69ecc0e76cd998485a
Tito Bouzout <tito.bouzout@gmail.com>
2011-05-13 07:22:08 -0300

    added little log to check functionality.

--- components/kGit.py
+++ components/kGit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import hashlib
+import logging
 from xpcom import components
 from xpcom.server import UnwrapObject
-    
+#qlog = logging.getLogger("KoPlaceTreeView.q")
+#qlog.setLevel(logging.DEBUG)
 class KGit:
     _com_interfaces_ = [components.interfaces.nsISupports,components.interfaces.IKGit]
     _reg_clsid_ = "{04e01b80-6eca-11e0-a1f0-0800200c9a66}"
@@ -31,5 +33,8 @@ class KGit:
         koFileObject = UnwrapObject(koFileObject)
         m = hashlib.md5()
         m.update(koFileObject.path.replace('\\', '/'))
+        #qlog.debug("koFile getter: file:%s, isLink:%r",
+        #              koFileObject.path.replace('\\', '/'),
+        #               m.hexdigest())
         properties.append('k'+m.hexdigest())#the css selector need start with a letter
         return properties
